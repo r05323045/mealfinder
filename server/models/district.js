cd289ddc8964bbd73b462175d800fb66aa6e2c11
iamy8000 <amy8000@gmail.com>
2021-01-29 20:08:43 +0800

    Add District model

---
+++ server/models/district.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class District extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  District.init({
+    name: DataTypes.STRING,
+    picture: DataTypes.STRING,
+    CityId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'District',
+  });
+  return District;
+};
