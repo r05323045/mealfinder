5b72145c3ea9d8b7f1b0287a47fad796cf390e56
r05323045 <r05323045@ntu.edu.tw>
2021-01-31 00:53:10 +0800

    init desktop layout of checkout success page

--- src/views/CheckoutSuccess.vue
+++ src/views/CheckoutSuccess.vue
@@ -1,47 +1,52 @@
 <template>
   <div class="checkout-page">
-    <div class="checkout-container">
-      <div class="main-title">付款成功！</div>
-      <div class="order-card">
-        <div class="header">
-          <div class="title">訂單摘要</div>
-        </div>
-        <div class="divider"></div>
-        <div class="card-content">
-          <div class="item-wrapper">
-            <div class="text">商品總計</div>
-            <div class="total-price">$1995</div>
+    <Navbar></Navbar>
+    <div class="checkout-result">
+      <div class="checkout-container">
+        <div class="main-title">付款成功！</div>
+        <div class="order-card-wrapper">
+          <div class="order-card">
+            <div class="header">
+              <div class="title">訂單摘要</div>
+            </div>
+            <div class="divider"></div>
+            <div class="card-content">
+              <div class="item-wrapper">
+                <div class="text">商品總計</div>
+                <div class="total-price">$1995</div>
+              </div>
+              <div class="item-wrapper">
+                <div class="text">數量總計</div>
+                <div class="total-price">5</div>
+              </div>
+              <div class="divider"></div>
+              <div class="item-wrapper summary">
+                <div class="text">結帳總金額</div>
+                <div class="total-price">$1995</div>
+              </div>
+            </div>
           </div>
-          <div class="item-wrapper">
-            <div class="text">數量總計</div>
-            <div class="total-price">5</div>
-          </div>
-          <div class="divider"></div>
-          <div class="item-wrapper summary">
-            <div class="text">結帳總金額</div>
-            <div class="total-price">$1995</div>
-          </div>
         </div>
-      </div>
-      <div class="illustration-wrapper">
-        <div class="cover">
-          <!--<div class="button">感謝你的使用！</div>-->
-        </div>
-      </div>
-      <div class="submit-button-wrapper">
-        <button class="submit-button" type="submit" @click.prevent="">
-          <div class="button">查看更多商品</div>
-        </button>
-        <div class="back-button" @click.prevent="">
-          <div class="button">回首頁看更多</div>
+        <div class="illustration-and-result">
+          <div class="title">訂位成功！</div>
+          <div class="illustration-wrapper">
+            <div class="cover">
+              <!--<div class="button">感謝你的使用！</div>-->
+            </div>
+          </div>
+          <div class="submit-button-wrapper">
+            <button class="submit-button" type="submit" @click.prevent="">
+              <div class="button">查看更多商品</div>
+            </button>
+            <div class="back-button" @click.prevent="">
+              <div class="button">回首頁看更多</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <div ref="footer">
       <Footer></Footer>
-    </div>
-    <div>
-      <Navbar></Navbar>
     </div>
   </div>
 </template>
@@ -81,130 +86,169 @@ $red: rgb(255, 56, 92);
 .checkout-page {
   height: 100vh;
   overflow: scroll;
-  .checkout-container {
-    margin: 36px 24px 60px 24px;
-    text-align: left;
-    position: relative;
-    .main-title {
-      font-size: 24px;
-      font-weight: 800;
-      line-height: 1.5;
-      margin-bottom: 24px;
+  .checkout-result {
+    scroll-behavior: smooth;
+    padding: 22px 24px;
+    @media (min-width: 992px) {
+      margin-top: 0px;
+      padding: 48px 80px;
     }
-    .order-card {
-      margin-bottom: 24px;
-      border: 1px solid $divider;
-      border-radius: 12px;
-      box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 16px;
-      .header {
-        border-top-left-radius: 12px;
-        border-top-right-radius: 12px;
-        padding: 15px;
-        background: #000000;
-        .title {
-          font-size: 16px;
-          color: #ffffff;
-          font-weight: 600;
+    .checkout-container {
+      display: flex;
+      flex-direction: column;
+      @media (min-width: 992px) {
+        flex-direction: row;
+      }
+      .main-title {
+        margin: 12px 0;
+        height: 40px;
+        font-size: 22px;
+        font-weight: 700;
+        text-align: left;
+        line-height: 22px;
+        @media (min-width: 992px) {
+          display: none;
         }
       }
-      .divider {
-        height: 1px;
-        background: $divider;
-      }
-      .card-content {
-        padding: 16px 16px 32px 16px;
-        .item-wrapper {
+      .order-card-wrapper {
+        min-width: 300px;
+        flex: 1;
+        @media (min-width: 992px) {
+          padding-top: 60px;
+        }
+        .order-card {
           margin-bottom: 24px;
-          font-size: 16px;
-          font-weight: 600;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-between;
-        }
-        .divider {
-          height: 1px;
-          background: $divider;
-          margin: 24px -16px;
-        }
-        .item-wrapper.summary {
-          font-size: 20px;
-          margin-bottom: 0;
-        }
-      }
-    }
-    .illustration-wrapper {
-      margin: 36px 0;
-      width: 100%;
-      padding-top: 66.7%;
-      position: relative;
-      background: url(../assets/bookingSuccess.svg) no-repeat center;
-      background-size: cover;
-      .cover {
-        position: absolute;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        .button {
-          padding: 12px 36px;
-          border-radius: 30px;
-          background: #000000;
-          color: #ffffff;
-          cursor: pointer;
-          font-weight: 700;
-          font-size: 16px;
-          line-height: 20px;
+          border: 1px solid $divider;
+          border-radius: 12px;
+          box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 16px;
+          .header {
+            border-top-left-radius: 12px;
+            border-top-right-radius: 12px;
+            padding: 15px;
+            background: #000000;
+            .title {
+              font-size: 16px;
+              color: #ffffff;
+              font-weight: 600;
+            }
+          }
+          .divider {
+            height: 1px;
+            background: $divider;
+          }
+          .card-content {
+            padding: 16px 16px 32px 16px;
+            .item-wrapper {
+              margin-bottom: 24px;
+              font-size: 16px;
+              font-weight: 600;
+              display: flex;
+              flex-direction: row;
+              justify-content: space-between;
+            }
+            .divider {
+              height: 1px;
+              background: $divider;
+              margin: 24px -16px;
+            }
+            .item-wrapper.summary {
+              font-size: 20px;
+              margin-bottom: 0;
+            }
+          }
         }
       }
-    }
-    .submit-button-wrapper {
-      border-top: 1px solid $divider;
-      width: calc(100vw - 48px);
-      background: #ffffff;
-      .submit-button {
-        border: none;
-        appearance: none;
-        margin-bottom: 20px;
-        height: 48px;
-        width: calc(100vw - 48px);
-        background: $red;
-        border-radius: 8px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        &:focus {
-          outline: none;
+      .illustration-and-result {
+        flex: 3;
+        @media (min-width: 992px) {
+          padding: 0 36px;
         }
-        .button {
-          color: #ffffff;
-          cursor: pointer;
-          font-weight: 600;
-          font-size: 16px;
-          line-height: 20px;
+        .title {
+          display: none;
+          @media (min-width: 992px) {
+            display: block;
+            margin-bottom: 24px;
+            font-size: 32px;
+            font-weight: 700;
+            text-align: center;
+            line-height: 36px;
+          }
         }
-      }
-      .back-button {
-        border: 1px solid #222222;
-        margin-bottom: 12px;
-        height: 46px;
-        width: calc(100vw - 48px);
-        background: #ffffff;
-        border-radius: 8px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        &:focus {
-          outline: none;
+        .illustration-wrapper {
+          margin: 36px 0;
+          width: 100%;
+          padding-top: 50%;
+          position: relative;
+          background: url(../assets/people-eating-food.svg) no-repeat center;
+          background-size: contain;
+          .cover {
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            .button {
+              padding: 12px 36px;
+              border-radius: 30px;
+              background: #000000;
+              color: #ffffff;
+              cursor: pointer;
+              font-weight: 700;
+              font-size: 16px;
+              line-height: 20px;
+            }
+          }
         }
-        .button {
-          color: #222222;
-          cursor: pointer;
-          font-weight: 400;
-          font-size: 16px;
-          line-height: 20px;
+        .submit-button-wrapper {
+          border-top: 1px solid $divider;
+          width: 100%;
+          background: #ffffff;
+          .submit-button {
+            border: none;
+            appearance: none;
+            margin-bottom: 20px;
+            height: 48px;
+            width: 100%;
+            background: $red;
+            border-radius: 8px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            &:focus {
+              outline: none;
+            }
+            .button {
+              color: #ffffff;
+              cursor: pointer;
+              font-weight: 600;
+              font-size: 16px;
+              line-height: 20px;
+            }
+          }
+          .back-button {
+            border: 1px solid #222222;
+            margin-bottom: 12px;
+            height: 46px;
+            width: 100%;
+            background: #ffffff;
+            border-radius: 8px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            &:focus {
+              outline: none;
+            }
+            .button {
+              color: #222222;
+              cursor: pointer;
+              font-weight: 400;
+              font-size: 16px;
+              line-height: 20px;
+            }
+          }
         }
       }
     }
