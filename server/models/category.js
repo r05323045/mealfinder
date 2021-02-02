fa0913246027741d36fb0307198fc4c7e7650b7f
iamy8000 <amy8000@gmail.com>
2021-02-02 15:01:31 +0800

    Feat: add association between Restaurant and Category

--- server/models/category.js
+++ server/models/category.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Category.hasMany(models.Restaurant)
     }
   };
   Category.init({
