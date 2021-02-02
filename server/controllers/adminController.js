bbbf2da0caef73ebe77bd5c2c5964eeda0972280
iamy8000 <amy8000@gmail.com>
2021-02-02 16:05:09 +0800

    Feat: include coupon at getRestaurants

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,10 +1,12 @@
 const sequelize = require('sequelize')
 const db = require('../models')
+const coupon = require('../models/coupon')
 const User = db.User
 const Category = db.Category
 const Restaurant = db.Restaurant
 const City = db.City
 const District = db.District
+const Coupon = db.Coupon
 
 const adminController = {
   getUsers: (req, res) => {
@@ -20,7 +22,7 @@ const adminController = {
     Restaurant.findAll({
       raw: true,
       nest: true,
-      include: [Category, City, District]
+      include: [Category, City, District, Coupon]
     })
     .then(restaurants => {
       return res.json({restaurants})
