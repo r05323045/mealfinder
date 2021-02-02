5e14cb4c4494cb5813f291e07668653dd260d71d
iamy8000 <amy8000@gmail.com>
2021-02-02 15:02:48 +0800

    Feat: add association between Restaurant and City

--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Restaurant.belongsTo(models.Category)
+      Restaurant.belongsTo(models.City)
     }
   };
   Restaurant.init({
