7a96bea50c7da76416a9ad547bef91fb6d13f035
iamy8000 <amy8000@gmail.com>
2021-02-02 17:49:32 +0800

    Remove column CategoryId from Users model

---
+++ server/migrations/20210202094650-remove-column-CategoryId-from-User.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'CategoryId')
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'CategoryId')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
