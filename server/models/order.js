bdbf85b6bc9984000d92c86bb14bc6ab9731e5f3
iamy8000 <amy8000@gmail.com>
2021-02-02 20:52:31 +0800

    Feat: add association between orderItem and coupon, order

--- server/models/order.js
+++ server/models/order.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Order.belongsTo(models.User)
+      Order.hasMany(models.orderItem)
     }
   };
   Order.init({
