34a7fa0a06ea5219a9365695a6556c8bbf935182
iamy8000 <amy8000@gmail.com>
2021-02-05 00:20:22 +0800

    Feat: add isFavorited=false function to getRestaurants

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -1,5 +1,4 @@
 const db = require('../models')
-const favorite = require('../models/favorite')
 const Restaurant = db.Restaurant
 const Category = db.Category
 const City = db.City
@@ -12,29 +11,25 @@ const restaurantController = {
   getRestaurants: (req, res) => {
     let offset = 0
     const pageLimit = 24
+    let isFavorited = false //沒登入狀態
 
     if (req.query.page) {
       offset = (req.query.page - 1) * pageLimit
     }
-
     Restaurant.findAll({
       raw: true,
       nest: true,
       include: [Category, City, District, Coupon],
       offset: offset,
       limit: pageLimit
+    }).then(restaurants => {
+      const data = restaurants.map(restaurant => ({
+        ...restaurant,
+        description: restaurant.description.substring(0, 50),
+        isFavorited: isFavorited
+      }))
+      return res.json({ data })
     })
-      .then(restaurants => {
-        const data = restaurants.map(restaurant => {
-          if (restaurant.description === null) {
-            return restaurant
-          } else {
-            restaurant.description = restaurant.description.substring(0, 50)
-            return restaurant
-          }
-        })
-        return res.json({ data })
-      })
   },
   getRestaurant: (req, res) => {
     Restaurant.findByPk(req.params.restaurantId, {
