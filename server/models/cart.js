4f2e032c41712f4b084835a6d0f6f460518cb35c
Ahno Chen <ahnochen1029@gmail.com>
2021-02-21 16:22:56 +0800

    association cart and coupon models

--- server/models/cart.js
+++ server/models/cart.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Cart.belongsToMany(models.Coupon, {
+        through: models.CartItem,
+        foreignKey: 'CartId',
+        as: 'items'
+      })
     }
   };
   Cart.init({
