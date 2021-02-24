74ffdc41b7b313212fb9ab0978eaf110700e066b
r05323045 <r05323045@ntu.edu.tw>
2021-02-24 23:08:45 +0800

    fetch API at cart page

--- client/src/apis/carts.js
+++ client/src/apis/carts.js
@@ -2,8 +2,28 @@ import { apiHelper } from '../utils/helpers'
 const getToken = () => localStorage.getItem('token')
 
 export default {
+  getCart () {
+    return apiHelper.get('/cart', {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
   postCart (data) {
     return apiHelper.post('/cart', data, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  deleteCart (id) {
+    return apiHelper.delete(`/cart/${id}`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  addCartItem (id) {
+    return apiHelper.post(`/cartItem/${id}/add`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  reduceCartItem (id) {
+    return apiHelper.post(`/cartItem/${id}/reduce`, {
       headers: { Authorization: `Bearer ${getToken()}` }
     })
   }
