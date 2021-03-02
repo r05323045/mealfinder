775310dba1044a9c152bf9e5082aec2c6b3508b8
iamy8000 <amy8000@gmail.com>
2021-03-03 00:58:55 +0800

    add devServer setting

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   devServer: {
-    headers: { 'Access-Control-Allow-Origin': '*' }
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    proxy: {
+      '/api': {
+        target: 'https://mealfirst.herokuapp.com/'
+      }
+    }
   }
 }
