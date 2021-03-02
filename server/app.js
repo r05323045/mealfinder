ee583233e976c2815c5213bc62813897f4ac3d27
iamy8000 <amy8000@gmail.com>
2021-03-03 03:05:22 +0800

    modify production env setting

--- server/app.js
+++ server/app.js
@@ -51,7 +51,7 @@ require('./routes')(app)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'public')))
-  app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 
 app.listen(port, () => {
