50e84a9b53c7b77aa02ec1a9c87f196a213d997b
iamy8000 <amy8000@gmail.com>
2021-03-02 21:00:47 +0800

    add Procfile for npm start setting

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 const path = require('path')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
