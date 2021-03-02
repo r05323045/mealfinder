6b2833d885111f662c5064b1fffd4936facd482c
iamy8000 <amy8000@gmail.com>
2021-03-03 01:23:57 +0800

    fix coupon seeder

--- server/seeders/20210130052432-Coupon-seeders.js
+++ server/seeders/20210130052432-Coupon-seeders.js
@@ -1,5 +1,5 @@
 'use strict'
-const restaurantData = require('../restaurant-data.json')
+const restaurantData = require('../test-restaurant-data.json')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -10,7 +10,7 @@ module.exports = {
         NO: `AA${i * 10 + 1}`,
         RestaurantId: i * 10 + 1,
         price: Math.floor(Math.random() * 10) * 10 + 100,
-        description: `這是${restaurantData[i].name}的coupon！`,
+        description: `這是${restaurantData.data[i].name}的coupon！`,
         createdAt: new Date(),
         updatedAt: new Date()
       })
