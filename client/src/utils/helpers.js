20462e6cc2bf79102abe3aef054bd2a53da14e69
iamy8000 <amy8000@gmail.com>
2021-03-03 01:37:37 +0800

    modify baseURL

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-const baseURL = 'http://localhost:3000/api'
+// const baseURL = 'http://localhost:3000/api'
+const baseURL = 'https://mealfirst.herokuapp.com/'
 
 export const apiHelper = axios.create({
   baseURL,
