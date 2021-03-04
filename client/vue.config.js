523bbc87e31ded063893a6236e69df76121c1209
r05323045 <r05323045@ntu.edu.tw>
2021-03-05 01:24:34 +0800

    revise vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  publicPath: '/mealfinder/',
+  publicPath: '/',
   outputDir: path.resolve(__dirname, '../server/public'),
   devServer: {
     headers: { 'Access-Control-Allow-Origin': '*' }
