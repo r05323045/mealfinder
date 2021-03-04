090acf1c6b9eac25171e383e8c4471034837e6ca
r05323045 <r05323045@ntu.edu.tw>
2021-03-05 00:26:02 +0800

    Install pg and set server and client config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,7 @@
+const path = require('path')
 module.exports = {
   publicPath: '/mealfinder/',
+  outputDir: path.resolve(__dirname, '../server/public'),
   devServer: {
     headers: { 'Access-Control-Allow-Origin': '*' }
   }
