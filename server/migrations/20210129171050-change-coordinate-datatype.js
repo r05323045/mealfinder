7745a2a5740b22a34a4a1b28c293700e4368ea5e
iamy8000 <amy8000@gmail.com>
2021-03-05 03:25:26 +0800

    change datatype to text

--- server/migrations/20210129171050-change-coordinate-datatype.js
+++ server/migrations/20210129171050-change-coordinate-datatype.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn('Restaurants', 'coordinates', {
-      type: Sequelize.JSONDB,
+      type: Sequelize.TEXT,
     });
   },
 
