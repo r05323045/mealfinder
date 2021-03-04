ba8e17eceb3dd9cae30cf080b73c9372cca5500c
r05323045 <r05323045@ntu.edu.tw>
2021-03-05 00:50:21 +0800

    add static setting for heroku

--- server/app.js
+++ server/app.js
@@ -40,11 +40,17 @@ app.use((req, res, next) => {
 
 app.use(cors({
   origin: [
-    'http://localhost:8080'
+    'http://localhost:8080',
+    'https://mealradar.herokuapp.com/'
   ],
   credentials: true,
   exposedHeaders: ['set-cookie']
 }))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/public/')))
+  app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+}
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
