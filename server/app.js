7a869377902f85f19b8e3a1fcc54f59b8bd33144
iamy8000 <amy8000@gmail.com>
2021-03-06 13:19:08 +0800

    Fix: require /routes before production setting at app.js

--- server/app.js
+++ server/app.js
@@ -47,12 +47,13 @@ app.use(cors({
   exposedHeaders: ['set-cookie']
 }))
 
+require('./routes')(app)
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/public/')))
   app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
 }
 
-require('./routes')(app)
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
