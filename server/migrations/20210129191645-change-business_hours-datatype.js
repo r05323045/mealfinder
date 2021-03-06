2b6cf5ff9afc165c0225b6d2c550f2943d99dbbb
iamy8000 <amy8000@gmail.com>
2021-03-06 13:24:53 +0800

    Fix: business hours conflicts

--- server/migrations/20210129191645-change-business_hours-datatype.js
+++ server/migrations/20210129191645-change-business_hours-datatype.js
@@ -3,11 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn('Restaurants', 'business_hours', {
-<<<<<<< HEAD
       type: Sequelize.STRING(1234),
-=======
-      type: Sequelize.TEXT,
->>>>>>> merge-master-deploy
     });
   },
 
