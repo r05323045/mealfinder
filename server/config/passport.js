9dba55e94949f084deb8ff25285d85f0a25c5bae
r05323045 <r05323045@ntu.edu.tw>
2022-08-19 13:48:43 +0800

    [fix]set oauth callback url to env

--- server/config/passport.js
+++ server/config/passport.js
@@ -61,7 +61,7 @@ passport.use(new FacebookStrategy({
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/api/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 }, (accessToken, refreshToken, profile, done) => {
   console.log(profile)
   // return done(null, profile)
