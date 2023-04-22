d14be965f5a66928105be96e0b77518c6bbffaf5
r05323045 <r05323045@ntu.edu.tw>
2023-04-22 14:44:11 +0800

    reset out put dir

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,5 @@
-const path = require('path')
 module.exports = {
   publicPath: '/',
-  outputDir: path.resolve(__dirname, '/dist'),
   devServer: {
     headers: { 'Access-Control-Allow-Origin': '*' },
     proxy: {
