ed66b59ca92943d850809587a0616950b6a13c64
r05323045 <r05323045@ntu.edu.tw>
2023-04-22 14:15:59 +0800

    vercel read-only issue

--- server/routes/apis.js
+++ server/routes/apis.js
@@ -4,7 +4,7 @@ const passport = require('../config/passport')
 const path = require('path')
 
 const multer = require('multer')
-const upload = multer({ dest: path.join(__dirname, '../temp') })
+const upload = multer({ dest: path.join(__dirname, '../tmp') })
 
 // JWT
 const jwt = require('jsonwebtoken')
