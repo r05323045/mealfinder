28e6c20564db40d019680e8673b28e73a8d4e0c0
r05323045 <r05323045@ntu.edu.tw>
2023-04-22 14:12:58 +0800

    multer issue

--- server/routes/apis.js
+++ server/routes/apis.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
+const path = require('path')
 
 const multer = require('multer')
-const upload = multer({ dest: 'temp/' })
+const upload = multer({ dest: path.join(__dirname, '../temp') })
 
 // JWT
 const jwt = require('jsonwebtoken')
