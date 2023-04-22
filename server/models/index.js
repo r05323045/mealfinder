9fd3d815e213bfc0bb7c3861a3f7caadc6137dbc
r05323045 <r05323045@ntu.edu.tw>
2023-04-22 13:00:47 +0800

    [fix]Please install mysql2 package manually

--- server/models/index.js
+++ server/models/index.js
@@ -3,10 +3,15 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const mysql2 = require('mysql2');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+
+if (options.dialect === 'mysql') {
+  options.dialectModule = mysql2;
+}
 
 let sequelize;
 if (config.database_url) {
