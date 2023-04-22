776e03db1a37a82726c9f6e718b8dd0e8baa1bcf
r05323045 <r05323045@ntu.edu.tw>
2023-04-22 14:25:13 +0800

    move fir

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 module.exports = {
   publicPath: '/',
-  outputDir: path.resolve(__dirname, '../server/public'),
+  outputDir: path.resolve(__dirname, '/dist'),
   devServer: {
     headers: { 'Access-Control-Allow-Origin': '*' },
     proxy: {
