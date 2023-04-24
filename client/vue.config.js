2d241bc25ffe81871a4efdfa15da66df60bbbc80
r05323045 <r05323045@ntu.edu.tw>
2023-04-24 20:09:47 +0800

    apply prerender-spa-plugin

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,7 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+
 module.exports = {
   publicPath: '/',
   devServer: {
@@ -6,6 +10,29 @@ module.exports = {
       '/api': {
         target: 'https://mealfinder-server.vercel.app'
       }
+    }
+  },
+  configureWebpack (config) {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'dist'),
+          routes: [
+            '/',
+            '/about',
+            '/restaurants',
+            '/map',
+            '/booking',
+            '/booking/success',
+            '/coupons',
+            '/signin',
+            '/signup'
+          ],
+          renderer: new Renderer({
+            renderAfterDocumentEvent: 'render-event'
+          })
+        })
+      )
     }
   }
 }
